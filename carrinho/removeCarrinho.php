ff17fe17c89593a77a0f6fe1a0f44d57521aa1ff
Victor Hugo <freitavitor71@gmail.com>
2025-09-10 19:53:12 -0300

    script de remoção de itens do carrinho
    Iniciando a criação do script para remover itens do carrinho dos clientes

---
+++ carrinho/removeCarrinho.php
@@ -0,0 +1,28 @@
+<?php
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: OPTIONS, DELETE");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
+    header("Content-Type: application/json; charset=UTF8");
+
+    if($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+        http_response_code(200);
+        exit;
+    }
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "E-commerce";
+
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch(PDOException $e) {
+        http_response_code(500);
+        echo json_encode(['mensagem' => 'Erro ao conectar ao banco de dados']);
+        exit;
+    }
+?>

a1a2be3bcba75efc90597b261634d3e4d2369d40
Victor Hugo <freitavitor71@gmail.com>
2025-11-24 20:41:03 -0300

    Envio e recebimento do MP

--- checkout/criarPreferenciaMP.php
+++ checkout/criarPreferenciaMP.php
@@ -28,8 +28,113 @@
 
     require '../vendor/autoload.php';
 
+    $dados = json_decode(file_get_contents('php://input'), true);
+    $idPedido = $dados['idPedido'];
+
+    if(!$idPedido) {
+        http_response_code(400);
+        echo json_encode(['mensagem' => 'ID do Pedido não fornecido']);
+        exit;
+    }
+
+    try {
+        $stmt = $conn->prepare("
+            SELECT 
+                ped.Valor_Total, cli.Email, cli.Nome, ipe.Id_Produto, 
+                ipe.Preco_Unitario, ipe.Quantidade, prod.Nome_Produto
+            FROM  
+                Pedidos ped
+            JOIN
+                Clientes cli ON ped.Id_Cliente = cli.Id_Cliente
+            JOIN
+                Itens_Pedido ipe ON ped.Id_Pedido = ipe.Id_Pedido
+            WHERE
+                ped.Id_Pedido = ?");
+
+        $stmt->execute([$idPedido]);
+        $resultados = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        
+    } catch(PDOException $e) {
+        http_response_code(500);
+        echo json_encode(['mensagem' => 'Erro ao consultar o pedido']);
+        exit;
+    }
+
+    if(empty($resultados)) {
+        http_response_code(404);
+        echo json_encode(['mensagem' => 'Pedido não encontrado ou sem itens']);
+        exit;
+    }
+
+    $itensMP = [];
+    $dadosCliente = [];
+
+    foreach($resultados as $row) {
+        if(empty($dadosCliente)) {
+            $dadosCliente = [
+                'email' => $row['Email'],
+                'nome' => $row['Nome'],
+                'valor_total' => $row['Valor_Total'],
+                'Telefone' => $row['Telefone']
+            ];
+        }
+
+        $itensMP[] = [
+            'id' => (string)$row['Id_Produto'],
+            'title' => $row['Nome_Produto'],
+            'description' => $row['Nome_Produto'],
+            'quantity' => (int)$row['Quantidade'],
+            'unit_price' => (float)$row['Preco_Unitario'],
+            'currency_id' => 'BRL'
+        ];
+    }
+
     use MercadoPago\Client\Preferencce\PreferenceClient;
     use MercadoPago\MercadoPagoConfig;
 
-    $key = $_ENV['ACESS_TOKEN_MP'];
+    $ACESS_TOKEN = $_ENV['ACESS_TOKEN_MP'];
+
+    MercadoPagoConfig::setAccessToken($ACESS_TOKEN);
+
+    $preferenceClient = new PreferenceClient();
+
+    $preferenceData = [
+        'items' => $itensMP,
+        'payer' => [
+            'email' => $dadosCliente['email'],
+            'name' => $dadosCliente['nome'],
+            'phone' => [
+                'area_code' => substr($dadosCliente['Telefone'], 0, 2),
+                'number' => substr($dadosCliente['Telefone'], 2)
+            ]
+        ],
+        'back_urls' => [
+            'sucess' => 'http://localhost:4200/checkout/sucesso',
+            'pending' => 'http://localhost:4200/checkout/falha',
+            'failure' => 'http://localhost:4200/checkout/pendente',
+        ],
+
+        'auto_return' => 'approved',
+
+        'external_return' => (string)$idPedido,
+
+        'notification_url' => '',
+    ];
+
+    try {
+        $preference = $preferenceClient->create($preference_data);
+
+        http_response_code(200);
+        echo json_encode([
+            'mensagem' => 'Preferência criada com sucesso',
+            'idPedidoInterno' => $idPedido,
+            'init_point' => $preference->init_point
+        ]);
+    } catch(\Exception $e) {
+        http_response_code(500);
+        echo_json_encode([
+            'mensagem' => 'Erro interno ao comunicaar com o Mercadoo Pago.',
+            'detalhes' => $e->getMessage()
+        ]);
+    }
 ?>
